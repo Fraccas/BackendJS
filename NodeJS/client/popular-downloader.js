3d69c1116aa56c788c401fc72f67bbf2f976fd94
Fraccas <daltonward1@yahoo.com>
2019-08-15 13:44:06 -0500

    Finished NodeJS Lab

---
+++ NodeJS/client/popular-downloader.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const fs = require('fs');
+const request = require('request');
+
+let dataPath = path.join(__dirname, '../downloads/');
+
+// get all popular articles from reddit
+request('https://reddit.com/r/popular.json', (err, res, body) => {
+    if (err) console.log(err);
+    
+    // loop through each json article 
+    JSON.parse(body).data.children.forEach(item => {
+        let art = item.data.url;
+        let id = item.data.id;
+        // check if article is media
+        if (path.extname(art) === '.jpg' || path.extname(art) === '.gif' || path.extname(art) === '.png') {
+            request(art, {encoding: 'binary'}, (err, res, body) => {
+                // store media to downloads folder
+                console.log(item.data);
+                fs.writeFile(dataPath + id + path.extname(art), body, 'binary', err => {
+                    if (err) console.log(err);
+                });
+            });
+        }
+    });
+});
+
+// request(url, {encoding: 'binary'}, function(error, response, body) {
+//     fs.writeFile('downloaded.jpg', body, 'binary', function (err) {});
+//   });
