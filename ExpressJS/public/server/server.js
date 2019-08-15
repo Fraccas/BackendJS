665a5b42919df03e1a7ed8288edcd608b73a6952
Fraccas <daltonward1@yahoo.com>
2019-08-15 18:57:20 -0500

    Finished Lab

--- ExpressJS/public/server/server.js
+++ ExpressJS/public/server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+let fs = require('fs');
 const bodyParser = require('body-parser');
 
 let app = express();
@@ -13,15 +14,29 @@ app.use((req, res, next) => {
 
 // form parsing 
 app.use(bodyParser.urlencoded( {extended: false}));
-
+let dataPath = path.join(__dirname, '../form-data.json');
 app.post('/contact-form', (req, res) => {
-    console.log(req.body.email);
-    console.log(req.body.name);
-});
+    // tried to write data to json, then output all data to res
+    // but couldn't get it
+    fs.readFile(dataPath, {encoding: "UTF-8"}, function (err, data) {
+        let jsonData = JSON.parse(data)
+        let newInfo = {name: req.body.name, email: req.body.email};
+        if (jsonData) jsonData.push(newInfo);
+        else jsonData = newInfo;     
+        console.log(jsonData);
 
-// app.get('/', (req, res) => {
-//      res.sendFile(path.join(__dirname, '../index.html'));
-// });
+        fs.writeFile(dataPath, JSON.stringify(jsonData), err => {
+            if (err) console.log(err);
+        });
+        res.write('Thanks for your info, ' + req.body.name + '!');
+        res.write('\nEmail: ' + req.body.email);
+
+        jsonData.forEach(element => {
+            res.write('\n\nUser: ' + element.name + ' - ' + element.email);
+        });
+        res.end();
+    });
+});
 
 app.use(express.static(path.join(__dirname, '..')));
 
