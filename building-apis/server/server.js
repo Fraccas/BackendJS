ed4ff5e7cc3425ef030b1a2aece42ffe8e418fd4
Fraccas <daltonward1@yahoo.com>
2019-08-16 20:39:03 -0500

    Finished API Lab!!

--- building-apis/server/server.js
+++ building-apis/server/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const apiRouter = require('./routes');
+const path = require('path');
 
 let app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use('/api', apiRouter);
+
+// middleware
+app.use(express.static(path.join(__dirname, '../')));
+console.log(path.join(__dirname, "../"));
+
 
 app.listen(3000);
